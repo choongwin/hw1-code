6d9576bb4ccc45031e386ee555f9646908c668e9
choongwin <choongyiwin0621@gmail.com>
2025-04-04 12:15:38 +0800

    [2200094605 initial commit] 导入了所有的weight并且初始化了gpt2模型，可以直接使用预

--- pretrain/gpt2train.py
+++ pretrain/gpt2train.py
@@ -160,5 +160,47 @@ class GPT(nn.Module):
         return model
 
 # -----------------------------------------------------------------------------
-model = GPT.from_pretrained('gpt2')
-print("didn't crash yay!")
+num_return_sequences = 5
+max_length = 30
+model = GPT.from_pretrained('gpt2') #初始化
+model.eval()#eval用于当一个模型只是使用而非训练
+model.to('cuda')#可以使用gpu计算
+
+# prefix tokens 将输入token化
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")#这行转换成8个token
+tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8) 输入5次去预测句子
+x = tokens.to('cuda')
+
+# generate! right now x is (B, T) where B = 5, T = 8
+# set the seed to 42
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length: #每次都会增加一个column 给x，x是输入的token
+    # forward the model to get the logits
+    with torch.no_grad():
+        logits = model(x) # (B, T, vocab_size)
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size) 只取最后一个last column 的logit
+        # get the probabilities
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50 (huggingface pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1) #the token sampling always be top 50 and not that random
+        # select a token from the top-k probabilities
+        # note: multinomial does not demand the input to sum to 1
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        # append to the sequence
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+    
+#we ported all the weight , we initialize the gpt2 
